360de47c79af93ae0e87ebf822a000b731b94740
anas-hassan1 <anas.hassan1@vodafone.com>
2025-06-29 15:07:21 +0300

    feat, automation: add dev env configuration files

--- conf.sh
+++ conf.sh
@@ -1,4 +1,4 @@
-k create ns solar-app
+k create ns dev-solar-app
 
-k create secret generic mongo-uri -n solar-app\
+k create secret generic mongo-uri -n dev-solar-app\
   --from-literal=MONGO_URI="mongodb://<mongo-usr>:<mongo-pass>@mongodb.mongodb.svc.cluster.local:27017/solar-system?authSource=solar-system"
