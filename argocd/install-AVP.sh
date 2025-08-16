12a8cbbe714db391eef6d361e9b6204ebe898efd
anas-hassan1 <anas.hassan1@vodafone.com>
2025-08-16 16:14:46 +0300

    refactor: use sidecar approach as the argocd-cm is deprecated

--- argocd/install-AVP.sh
+++ argocd/install-AVP.sh
@@ -8,21 +8,17 @@ vault read -namespace=argocd auth/approle/role/argocd-role/role-id
 vault write -namespace=argocd auth/approle/role/argocd-role policies=argocd-policy
 vault write -f -namespace=argocd auth/approle/role/argocd-role/secret-id
 
-
+kubectl create secret generic vault-ca --from-file=vault-ca.crt=./ca.crt -n argocd
 # create a k8s secret with the role-id and secret-id
 k create secret generic avp-creds --from_env_file=creds.txt -n argocd
 # check the created secret
 k get secret -n argocd avp-creds -o json | jq -r .data.VAULT_ADDR | base64 -d
 
-# Edit the argocd-repo-server deployment to add the argocd-vault-plugin
+# backup the current argocd-repo-server deployment
 k get -n argocd deploy/argocd-repo-server -o yaml > argocd-repo-server-deploy.yaml
 
-# 1- add a volume named custom-tools with emptyDir
-# 2- create another init container to download the argocd-vault-plugin binary by mounting the custom-tools volume
-#    and copying the binary to /usr/local/bin/argocd-vault-plugin
-# 3- mount the custom-tools volume to the argocd-repo-server container at /usr/local/bin/argocd-vault-plugin
-# 4- add the envFrom directive to the argocd-repo-server container to use the avp-creds secret
-
+# 1- create a configurationManagementPlugin configmap for argocd
+k apply -f cmp-plugin.yaml
 k apply -f argocd-repo-server-deploy.yaml
 
 # register the argocd-vault-plugin with argocd-cm
